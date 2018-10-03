9208fc172b7239f9c3bd0f5badf1ee22909e7d2c
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-03 03:21:46 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
-    category_attributes.values.each do [category_attribute]
+    category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
       self.post_categories.build(category: category)
     end
